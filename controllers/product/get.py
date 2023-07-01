e06ba91e44aac0a68639146a2ee038a97b3092c2
Ishan Shishodiya <sly.of.zero@gmail.com>
2023-07-02 01:53:28 +0530

    Fixed product page

--- controllers/product/get.py
+++ controllers/product/get.py
@@ -1,8 +1,9 @@
 from flask import render_template
-from models import Product
+from models import Product, Category
 
 
 def product_get_controller(product_id, request):
     product = Product.query.filter_by(id=product_id).first()
+    category = Category.query.filter_by(id=product.category).first()
 
-    return render_template("product.html", product=product)
+    return render_template("product.html", product=product, category=category)
